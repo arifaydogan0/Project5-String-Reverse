92aa1425e8d0ae11cb9e40f020916b5671625d78
Arif Aydoğan <aydoganarif@yahoo.com>
2022-06-25 11:12:06 +0300

    Add project files.

---
+++ Project5-String Reverse/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+while (true)
+{
+    Console.WriteLine("Bir kelime giriniz: ");
+    string input = Console.ReadLine();
+    string output = "";
+    for (int i = input.Length - 1; i >= 0; i--)
+    {
+        output += input[i].ToString();
+    }
+    Console.WriteLine(output);
+    Console.WriteLine();
+}
